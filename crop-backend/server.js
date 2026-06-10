288a61793079eb664a9866dcca92e1b5860c98ee
Bharat Vishwakarma <bharatvishwakarma284@gmail.com>
2026-06-10 14:54:34 +0530

    Added Render PORT support

--- crop-backend/server.js
+++ crop-backend/server.js
@@ -541,6 +541,8 @@ app.post("/api/pesticide", (req, res) => {
 
 // ================= SERVER =================
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000 🚀");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
